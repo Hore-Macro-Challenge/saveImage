33ec5694486da41ee7d335d47a080a97d6d9a9f9
zzungokong <fantasania@gmail.com>
2023-10-16 10:48:38 +0700

    Initial Commit

---
+++ puzzle/puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  puzzleApp.swift
+//  puzzle
+//
+//  Created by Vania on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
